410b3daea53568f6d4764009a45bb05e899372f9
eric <xihh01@gmail.com>
2020-09-02 22:06:18 -0700

    send message

--- sendNews.py
+++ sendNews.py
@@ -47,3 +47,24 @@ def create_message_with_attachment(
     message.attach(msg)
 
     return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+
+    Returns:
+      Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(
+            userId=user_id, body=message).execute())
+        print 'Message Id: %s' % message['id']
+        return message
+    except errors.HttpError, error:
+        print 'An error occurred: %s' % error
+
