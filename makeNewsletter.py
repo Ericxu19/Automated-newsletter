6ba05348b14b0cfbee8e90d6a101d7cde064a4c5
eric <xihh01@gmail.com>
2020-09-03 12:44:19 -0700

    style change

--- makeNewsletter.py
+++ makeNewsletter.py
@@ -3,7 +3,7 @@ import pdfkit
 import pullLinks
 import re
 
-styles_str = "<style>p{font-size: 150%}body{font-size:150%}</style>"
+styles_str = "<style>p{font-size: 150%}body{font-size:150%}h4{font-size: 150%}div.blog-item-author-profile-wrapper{opacity: 0.0;height: 0px}div.blog-meta-item{opacity: 0.0;height: 0px;}</style>"
 
 with open("out.html", "ab") as f:
     f.write(styles_str.encode(encoding="utf-8"))
@@ -18,8 +18,11 @@ space = "<br>"
 links = pullLinks.pullLinks()
 with open("out.html", "ab") as f:
     fr = "<P> for further reading: </p><br>"
+    
     for link in links:
+        link = '<p>'+ link + '</p>'
         f.write(space.encode(encoding="utf-8"))
+        
         f.write(link.encode(encoding="utf-8"))
 
     
