35386d47e1ce0cb2d261f18e2b20bf276ec75b0d
eric <xihh01@gmail.com>
2020-09-03 14:46:11 -0700

    added intro

--- makeNewsletter.py
+++ makeNewsletter.py
@@ -5,26 +5,42 @@ import re
 
 styles_str = "<style>p{font-size: 150%}body{font-size:150%}h4{font-size: 150%}div.blog-item-author-profile-wrapper{opacity: 0.0;height: 0px}div.blog-meta-item{opacity: 0.0;height: 0px;}</style>"
 
+chapters = open("intro.html", "r").read()
 with open("out.html", "ab") as f:
     f.write(styles_str.encode(encoding="utf-8"))
     f.write("<head><meta charset='utf-8'></head>".encode(encoding="utf-8"))
     f.write("<img src='Banner.png' >".encode(encoding="utf-8"))
+    f.write("<p>August<p><br>".encode(encoding="utf-8"))
+    
 
+
+links = pullLinks.pullLinks()
+intro ='<p>'+ links[0]+ '<p>' 
+with open("out.html", "ab") as f:
+    f.write("Introduction: <br>".encode(encoding="utf-8"))
+    f.write(intro.encode(encoding="utf-8"))
+    f.write(chapters.encode(encoding="utf-8"))
 
 pullBlog.pullBlog()
 
 space = "<br>"
 
-links = pullLinks.pullLinks()
 with open("out.html", "ab") as f:
-    fr = "<P> for further reading: </p><br>"
-    
-    for link in links:
+    fr = "<P>For further reading: </p>"
+    f.write(fr.encode(encoding="utf-8"))
+    for link in links[1]:
         link = '<p>'+ link + '</p>'
         f.write(space.encode(encoding="utf-8"))
         
         f.write(link.encode(encoding="utf-8"))
 
-    
-pdfkit.from_file('out.html', 'out.pdf')
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+}
+pdfkit.from_file('out.html', 'out.pdf', options = options)
 
--- pullLinks.py
+++ pullLinks.py
@@ -30,7 +30,7 @@ def pullLinks():
             pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
-
+    intro = ''
     # Call the Gmail API
     messageList = service.users().messages().list(userId='me').execute()
     messageList = messageList['messages']
@@ -41,4 +41,8 @@ def pullLinks():
                 # 
                 if dic['value'] == "link"+ "8":
                     links.append(m['snippet'])
-    return links
+                if dic['value'] == "intro"+ "8":
+                    intro= "For this month, we start with Justin Stein's analysis of how Wall Street is oblivious to what is happening on Main street, continues with environmental consultants' Racheal Fleishaiman's pled to society that future environmental policy is guided by the mistakes we have made in combating COVID 19, then continues to Judith Langer's sobering portrait of the predictable yet heartbreaking increase of abuse and violence during pandemic lockdowns, and concludes with John Willis' recounting of the indignities he suffered as a blind person trying to get a COVID test recently in Toronto."
+
+                    
+    return [intro, links]
