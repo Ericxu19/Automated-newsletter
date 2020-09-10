b50e36defc5a2c7959175aff19d1bf7964994424
eric <xihh01@gmail.com>
2020-09-09 17:47:19 -0700

    new link format

--- makeNewsletter.py
+++ makeNewsletter.py
@@ -9,7 +9,7 @@ styles_str = "<style>p{font-size: 150%}body{font-size:150%}h4{font-size: 150%}di
 with open("out.html", "ab") as f:
     f.write(styles_str.encode(encoding="utf-8"))
     f.write("<head><meta charset='utf-8'></head>".encode(encoding="utf-8"))
-    f.write("<img src='Banner.png' >".encode(encoding="utf-8"))
+    f.write("<img src='https://images.squarespace-cdn.com/content/v1/5ecd504cf4aac80c66bc8fa0/1599676234805-Z5KJ2ZC6O5YEWHJT6YD6/ke17ZwdGBToddI8pDm48kDLZMA5A_0QTt70JA05UypMUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dsn8kDoUsqm_RvvYkNOfzP_cCjuFVRfEFw1RcssrXSLJCjLISwBs8eEdxAxTptZAUg/Banner.png?format=1000w' >".encode(encoding="utf-8"))
     f.write("<h1>August 2020</h1>".encode(encoding="utf-8"))
     
 
@@ -24,12 +24,13 @@ with open("out.html", "ab") as f:
         f.write(('<p>'+ str(i+1)+' '+blog[i][0]+ '</p>').encode())
 
         f.write(('<p style="margin-left: 1em">' + blog[i][1]+ '</p>').encode())
-        
+        #f.write((str(i)+' '+blog[i][0]+'/n').encode(encoding="utf-8"))
     f.write(('<p>'+ str(len(blog)+1)+' For further reading'+ '</p>').encode())
-    
+    #f.write(chapters.encode(encoding="utf-8"))
     f.write('<hr style="height:2px;border:none;color:#333;background-color:#333;">'.encode())
     for b in blog:
         f.write(b[2].encode(encoding="utf-8"))
+        f.write('<hr style="height:2px;border:none;color:#333;background-color:#333;">'.encode())
         
 
 
@@ -39,7 +40,9 @@ with open("out.html", "ab") as f:
     fr = "<h1>FOR FURTHER READING </h1>"
     f.write(fr.encode(encoding="utf-8"))
     for link in links[1]:
-        link = '<p>'+ link + '</p>'
+        actualLink = re.findall(r'http.*$', link)[0]
+        link = re.sub(r'http.*$','', link)
+        link = '<p><a href=' + actualLink +  '><span style="text-decoration:underline">' + link + '</span></a></p>'
         
         f.write(link.encode(encoding="utf-8"))
 
